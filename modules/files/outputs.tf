2a5c3a857dc27a484a274098fc6c0fb77f4e08d9
patakitamas2002 <patakitamas2002@gmail.com>
2023-05-25 12:07:08 +0200

    fix terraform plan

--- modules/files/outputs.tf
+++ modules/files/outputs.tf
@@ -1,3 +1,6 @@
 output "file_content_md5" {
-  value = local_file.file[0].content_md5
+  value = local_file.felad1[0].content_md5
+}
+output "first_file_name"{
+  value = local_file.felad1[0].filename
 }
--- modules/read/outputs.tf
+++ modules/read/outputs.tf
@@ -1,6 +1,6 @@
 output "file_content_md5" {
-  value = "read-${module.fileCreator.file_content_hashed_md5}"
+  value = "read-${module.makeFile.file_content_md5}"
 }
 output "first_file_name" {
-  value = module.fileCreator.first_file_name
+  value = module.makeFile.first_file_name
 }
